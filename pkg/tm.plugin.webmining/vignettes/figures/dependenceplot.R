78a71f38cf2b9cb62fe719dcd4894560394a20ab
mannau <mannau@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-04-02 19:45:15 +0000

    dependenceplot update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sentiment@38 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tm.plugin.webmining/vignettes/figures/dependenceplot.R
+++ pkg/tm.plugin.webmining/vignettes/figures/dependenceplot.R
@@ -11,8 +11,8 @@ labels <- c("tm.plugin.webmining", "tm", "boilerpipeR",  "RCurl" ,"XML", "RJSONI
 par(mar=c(0,0,0,0))
 plot.new()
 elpos<-coordinates (c(1,5))
-treearrow(from=elpos[1,],to=elpos[2:3,],arr.side=2,arr.pos=0.7,path="H")
-treearrow(from=elpos[1,],to=elpos[3:6,],arr.side=2,arr.pos=0.7,path="H", lty = 2)
+treearrow(from=elpos[1,],to=elpos[2:5,],arr.side=2,arr.pos=0.7,path="H")
+treearrow(from=elpos[1,],to=elpos[6,],arr.side=2,arr.pos=0.7,path="H", lty = 2)
 textrect (elpos[1,],0.2,0.05,lab=labels[1],cex=1, , shadow.size = 0, font = 2)
 for ( i in 2:6){
 	textrect (elpos[i,],0.1,0.05,lab=labels[i],cex=1, shadow.size = 0)
