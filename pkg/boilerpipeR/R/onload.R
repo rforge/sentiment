2068e4cd0abba4637a3c6b2e36eaf13e6ab78982
mannau <mannau@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-04-07 09:18:12 +0000

    remove onload man file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sentiment@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/boilerpipeR/R/onload.R
+++ pkg/boilerpipeR/R/onload.R
@@ -1,5 +1,6 @@
 #'Init function
 #' @importFrom rJava .jpackage
+#' @noRd
 .onLoad <-
 function(libname, pkgname){
     .jpackage(pkgname, lib.loc = libname)
--- pkg/tm.plugin.webmining/R/getLinkContent.R
+++ pkg/tm.plugin.webmining/R/getLinkContent.R
@@ -1,58 +1,3 @@
-# TODO: Add comment
-# 
-# Author: mario
-###############################################################################
-
-#getURLTimelimit <- function(urls, timeout, curlOpts = 
-#				curlOptions(followlocation = TRUE, 
-#				maxconnects = 20,
-#				maxredirs = 10,
-#				timeout = timeout,
-#				connecttimeout = timeout),...){
-#	
-#	setTimeLimit(cpu=timeout, elapsed=timeout, transient=TRUE);
-#	getURL(urls, timelimit, curlOpts = curlOpts, ...)
-#}
-
-#evalWithTimeout <- 
-#function(..., envir=parent.frame(), timeout, cpu=timeout, elapsed=timeout, onTimeout=c("error", "warning", "silent")) {
-#	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-#	# Validate arguments
-#	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-#	# Argument 'cpu' and 'elapsed':
-#	cpu <- Arguments$getNumeric(cpu, range=c(0,Inf));
-#	elapsed <- Arguments$getNumeric(elapsed, range=c(0,Inf));
-#	
-#	# Argument 'onTimeout':
-#	onTimeout <- match.arg(onTimeout);
-#	
-#	
-#	# Default result value
-#	res <- invisible();
-#	
-#	setTimeLimit(cpu=cpu, elapsed=elapsed, transient=TRUE);
-#	tryCatch({
-#				res <- eval(..., envir=envir);
-#			}, error = function(ex) {
-#				msg <- ex$message;
-#				# Was it a timeout?
-#				pattern <- gettext("reached elapsed time limit");
-#				if (regexpr(pattern, msg) != -1) {
-#					ex <- TimeoutException(msg, cpu=cpu, elapsed=elapsed);
-#					if (onTimeout == "error") {
-#						throw(ex);
-#					} else if (onTimeout == "warning") {
-#						warning(getMessage(ex));
-#					} else if (onTimeout == "silent") {
-#					}
-#				} else {
-#					# Rethrow error
-#					throw(ex);
-#				}
-#			})
-#	
-#	res;
-#}
 
 #' Get Corpus link content
 #' @param corpus corpus for which link content should be downloaded
@@ -62,53 +7,109 @@
 #' @param curlOpts curl options to be passed to \code{\link{getURL}}
 #' @param ... additional parameters to \code{\link{getURL}}
 #' @export
-getLinkContent <- function(corpus, links = sapply(corpus, meta, "Origin"),timeout.request = 30,
-		curlOpts = curlOptions(	followlocation = TRUE, 
-				maxconnects = 20,
+getLinkContent <- function(corpus, links = meta(corpus, "Origin"),
+		timeout.request = 30, chunksize = 20, verbose = getOption("verbose"),
+		curlOpts = curlOptions(	verbose = FALSE,
+				followlocation = TRUE, 
+				maxconnects = 5,
 				maxredirs = 10,
 				timeout = timeout.request,
 				connecttimeout = timeout.request,
+				ssl.verifyhost=FALSE,
 				ssl.verifypeer = FALSE,
-				useragent = "R"),...){
+				useragent = "R"),  
+		#write.disk = F, 
+		tdir = tempdir(), 
+		retry.empty = 3, 
+		delete.tempdir = F, 
+		sleep.time = 3, 
+		extractor = ArticleExtractor, ...){
 	
-	if(length(corpus) != length(links))
+	if(nrow(corpus) != length(links))
 		stop("Corpus length not equal to links length\n")
-
+	
 	#content_urls <- unlist(sapply(content_parsed, linkreader))
-	content <- tryCatch({
-				#cat("Get ", length(links), "Links...")
-				#timeout = timeout.request * length(links)
-				#setTimeLimit(cpu=timeout, elapsed=timeout, transient=TRUE)
-				getURL(links, .opts = curlOpts, ...)
-				},
-				error=function(e){
-					print(e)
-					cat("\nError on retrieval, single retrieval fallback... \n")
-					content <- list()
-					for(i in 1:length(links)){
-						content[[i]] <- tryCatch({
-								#timeout = timeout.request * length(links)
-								#setTimeLimit(cpu=timeout, elapsed=timeout, transient=TRUE)
-								getURL(links, .opts = curlOpts, ...)
-							},error = function(f) {
-												  print(f)
-												  ""})
-					}
-					#cat("Done\n")
-					do.call(c, content)})
-	#assign web content to 
-	for(i in 1:length(corpus)){
-		Content(corpus[[i]]) <- content[i]
+	if(verbose){
+		cat("Starting URL Download ...\n")
 	}
+	
+	retries <- 0
+	#allcontent <- rep("", length(links))
+	
+	while(any((tlength <- textlength(corpus))[,1] < 1) & (retries <= retry.empty)){
+		# Avoid memory leakages through parallel processing
+		p <- parallel({
+			retries <- retries + 1
+			emptycontent.ids <- as.numeric(row.names(tlength)[tlength[,1] < 1])
+			
+			if(verbose){
+				cat("Run ", retries, ", retrieving ", length(emptycontent.ids), " content items\n")
+			}
+			
+			#for(cstart in seq(from = 1, to =  length(links), by = chunksize)){
+			for(cstart in seq(from = 1, to =  length(emptycontent.ids), by = chunksize)){
+				if(sleep.time > 0){
+					if(verbose){
+						cat("Sleeping ", sleep.time, " seconds...\n")
+					}
+					Sys.sleep(sleep.time)
+				}
+				
+				cend <- min(cstart[1] + chunksize-1, length(emptycontent.ids))
+				chunk.ids <- emptycontent.ids[cstart:cend]
+				chunk <- links[chunk.ids]
+				
+				# TODO Enable chunk download
+				content <- tryCatch({
+							#cat("Get ", length(links), "Links...")
+							#timeout = timeout.request * length(links)
+							#setTimeLimit(cpu=timeout, elapsed=timeout, transient=TRUE)
+							getURL(chunk, .opts = curlOpts, ...)
+						},
+						error=function(e){
+							print(e)
+							# TODO: Check if really necessary
+							cat("\nError on retrieval, single retrieval fallback... \n")
+							content <- list()
+							for(i in 1:length(chunk)){
+								content[[i]] <- tryCatch({
+											#timeout = timeout.request * length(links)
+											#setTimeLimit(cpu=timeout, elapsed=timeout, transient=TRUE)
+											getURL(chunk[i], .opts = curlOpts, ...)
+										},error = function(f) {
+											print(f)
+											""})
+							}
+							#cat("Done\n")
+							do.call(c, content)})
+				
+				
+				# Extract Content
+				extract <- sapply(content, extractor, ...)
+				
+				# Escape '
+				#extract <- gsub("'", "", extract)	
+				
+				# Put Content Into Database
+				Content(corpus[chunk.ids,]) <- extract
+				
+				
+				#allcontent[chunk.ids] <- content
+				
+				if(verbose){
+					progress <- floor(cend/length(links)*100)
+					cat(paste(progress, "% (",cend,"/",length(emptycontent.ids), ") ", Sys.time(), "\n",sep = ""))
+				}
+				closeAllConnections()
+				
+			}
+			TRUE
+		})
+		success <- collect(p)
+	}
+#	if(length(corpus) != length(allcontent)){
+#		stop("Length mismatch: length(Corpus) != length(allcontent)")
+#	}
 	
 	corpus
 }
-
-#Define as Method for Corpus
-## retrieve Content from empty corpus items
-## @export
-#retrieveEmpty <- function(corpus, ...){
-#	noContent <- sapply(corpus, nchar) == 0
-#	corp_nocontent <- getLinkContent(corpus[noContent], ...)
-#	c(corpus[!noContent],corp_nocontent)
-#}
